a262ed07a1164cfe15fdd289673aa89c8113d923
areesha <areeshasayed09@gmail.com>
2022-10-29 13:50:02 +0530

    updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import houseimg from "./house.jpg"
+
 import {IconHeart,IconAnalyze, IconBath,IconBed } from "@tabler/icons"
-const Card = ({price, imgs, property, address, bed, bath, area  }) => {
+const Card = ({price, imgs, date, property, address, bed, bath, area  }) => {
   return (
     <div className='card'>
         <img src={imgs} className='w-full h-1/2 rounded-md' alt="house" />
         <div className='flex mt-2'>
             <div>
-            <span className='ml-5 text-indigo-400 font-semibold'>${price}</span> <span className='text-neutral-300 text-xs'>/month</span>
+            <span className='ml-5 text-indigo-400 text-xl font-semibold'>${price}</span> <span className='text-neutral-300 text-xs'>/month</span>
             </div>
-             <button className=' flex justify-center items-center ml-auto mr-5 border-2 border-solid border-neutral-900 rounded-full w-8 h-8'><IconHeart/></button>
+             <button className=' flex justify-center items-center ml-auto mr-5 border-2 border-solid border-indigo-600 rounded-full w-8 h-8'><IconHeart color='rgb(79 70 229)'/></button>
         </div>
 
-        <div className='w-full flex'>
-            <span className='text-lg font-bold items-end ml-5'>{property}</span>
+        <div className='w-full flex item-center'>
+            <span className='text-lg font-bold items-start ml-5'>{property}</span>
+            <span className='text-xs ml-auto mr-3 mt-1'>Availabe:{date}</span>
         </div>
         
         <div className='w-full flex mb-3'>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import DateState from './context/DateState';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DateState>
+       <App />
+    </DateState>
   </React.StrictMode>
 );
 
